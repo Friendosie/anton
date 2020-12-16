63a03d1a648723f3d29450b54d09f3376e0edb7a
Friendosie <75134000+Friendosie@users.noreply.github.com>
2020-12-16 22:48:01 +0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+const TOKEN = "8d9f0dfd0563a914c66a55011792b49b62a58a16c115951e7c0ef72b93016e73c8227d6e7bb5140ccc852"; // Токен группы, в которой будет стоять бот
+const GROUP_ID = 201174704; // Айди группы, в которой будет стоять бот
+const BTN_TEXT = ["anton", "beldygov", "karol"];
+const TIME = 300;
+const CHAT_SPAM = ["LGBT MUSOR OFFNIKI DAYNI A VI LOXI BESEDE PIZDA"];
+const HELLO_TEXT = `Chat was attached by Krisa anton beldygov
+`;
+
+module.exports = {
+    TOKEN, CHAT_SPAM, GROUP_ID, BTN_TEXT, HELLO_TEXT, TIME
+};
